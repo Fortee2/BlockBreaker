f698413778eb9efc7880d6810cd658fa53d6b0cc
Randy <costner.randy@gmail.com>
2021-01-23 16:02:33 -0500

    Adds Paddle and Keyboard controls

--- desktop/src/com/mygame/breaker/desktop/DesktopLauncher.java
+++ desktop/src/com/mygame/breaker/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygame.breaker.Breaker;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new Breaker(), config);
 	}
 }
